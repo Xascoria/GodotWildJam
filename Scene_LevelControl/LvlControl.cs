c70de9143d7b0cf2071524fee6f216c0c9730daa
Xascoria <59613709+Xascoria@users.noreply.github.com>
2021-02-14 20:33:59 +0800

    Minor improvements

--- Scene_LevelControl/LvlControl.cs
+++ Scene_LevelControl/LvlControl.cs
@@ -6,16 +6,41 @@ public class LvlControl : Node
 	Terminal terminal;
 	Display display;
 
-	private int story_progress = -1;
+	Tween tween;
+	Timer timer1;
+	Timer timer2;
+
+	private int story_progress = 0;
 
 	public override void _Ready()
 	{
 		terminal = GetNode<Terminal>("../Terminal");
 		display = GetNode<Display>("../Display");
+		tween = GetNode<Tween>("Tween");
+		timer1 = GetNode<Timer>("Timer");
+		timer2 = GetNode<Timer>("Timer2");
+
+		SetupInitial();
 	}
 
 	private void SetupInitial()
 	{
+		terminal.Modulate = new Color(1,1,1,0);
+		display.Modulate = new Color(1,1,1,0);
+	}
 
+	private void _on_Tween_tween_all_completed()
+	{
+		// Replace with function body.
+	}
+
+	private void _on_Timer_timeout()
+	{
+		// Replace with function body.
 	}
 }
+
+
+
+
+
