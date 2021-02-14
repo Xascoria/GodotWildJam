5a1dee4822578c84ac50b3eb0aa203e563271f08
Xascoria <59613709+Xascoria@users.noreply.github.com>
2021-02-14 19:37:03 +0800

    Second Commit

--- Scene_LevelControl/LvlControl.cs
+++ Scene_LevelControl/LvlControl.cs
@@ -3,10 +3,19 @@ using System;
 
 public class LvlControl : Node
 {
-	
+	Terminal terminal;
+	Display display;
+
+	private int story_progress = -1;
 
 	public override void _Ready()
 	{
-		
+		terminal = GetNode<Terminal>("../Terminal");
+		display = GetNode<Display>("../Display");
+	}
+
+	private void SetupInitial()
+	{
+
 	}
 }
